9b26b323d0d8f8bbc8fc97a3fdc0764d20d75960
vang007 <84373534+vang007@users.noreply.github.com>
2022-04-09 00:55:45 -0500

    started

--- utils/inputCheck.js
+++ utils/inputCheck.js
@@ -2,20 +2,20 @@
 // e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
 
 module.exports = function(obj, ...props) {
-  const errors = [];
+    const errors = [];
 
-  props.forEach((prop) => {
-    // if property is blank or doesn't exist, add to errors array
-    if (obj[prop] === undefined || obj[prop] === '') {
-      errors.push(`No ${prop} specified.`);
+    props.forEach((prop) => {
+        // if property is blank or doesn't exist, add to errors array
+        if (obj[prop] === undefined || obj[prop] === '') {
+            errors.push(`No ${prop} specified.`);
+        }
+    });
+
+    if (errors.length) {
+        return {
+            error: errors.join(' ')
+        };
     }
-  });
 
-  if (errors.length) {
-    return {
-      error: errors.join(' ')
-    };
-  }
-  
-  return null;
+    return null;
 };
