833d96fb848d6f2c32e54276dea869c11450b3c4
vang007 <84373534+vang007@users.noreply.github.com>
2022-04-26 03:10:30 -0500

    finished ALTER, DROP, JOIN STATEMENTS

--- server.js
+++ server.js
@@ -11,17 +11,15 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection({
-        host: 'localhost',
-        // MySQL username
-        user: 'vang007',
-        // MYSQl password
-        password: 'Zuzu2020$',
-        database: 'election'
-    },
-    console.log('Connected to the election database.')
-);
+    host: 'localhost',
+    // MySQL username
+    user: 'vang007',
+    // MYSQl password
+    password: '',
+    database: 'election'
+});
 
-// Get all candidates
+// Get all candidates and party affiliation
 app.get('/api/candidates', (req, res) => {
     const sql = `SELECT candidates.*, parties.name
     AS party_name
@@ -38,6 +36,29 @@ app.get('/api/candidates', (req, res) => {
             message: 'success',
             data: rows
         });
+    });
+});
+
+// Update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+    const sql = `UPDATE candidates SET party_id = ? 
+                WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            // check if a record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
     });
 });
 
@@ -62,6 +83,8 @@ app.get('/api/candidate/:id', (req, res) => {
         });
     });
 });
+
+
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
@@ -101,7 +124,6 @@ app.post('/api/candidate', ({ body }, res) => {
     const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
     VALUES (?,?,?)`;
     const params = [body.first_name, body.last_name, body.industry_connected];
-
     db.query(sql, params, (err, result) => {
         if (err) {
             res.status(400).json({ error: err.message });
@@ -121,4 +143,71 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
+
+// Get all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// Get single party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// Delete parties
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            // checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+// Not Found response for unmatched routes
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
